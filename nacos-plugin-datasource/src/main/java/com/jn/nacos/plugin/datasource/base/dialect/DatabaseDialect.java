acee15f2143a254846c52c045c83315a456ad549
coderjn <fs1194361820@163.com>
2024-08-23 08:56:27 +0800

    * 设计 database plugin 雏形

--- nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/dialect/DatabaseDialect.java
+++ nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/dialect/DatabaseDialect.java
@@ -1,5 +1,33 @@
 package com.jn.nacos.plugin.datasource.base.dialect;
 
-public interface DatabaseDialect {
-    String getFunction(String functionName);
+import com.jn.sqlhelper.dialect.Dialect;
+import com.jn.sqlhelper.dialect.SQLDialectException;
+import com.jn.sqlhelper.dialect.pagination.PagedPreparedParameterSetter;
+import com.jn.sqlhelper.dialect.pagination.QueryParameters;
+import com.jn.sqlhelper.dialect.pagination.RowSelection;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public abstract class DatabaseDialect {
+
+    public abstract String getDatabase();
+    public abstract String getFunction(String functionName);
+
+    protected Dialect dialect;
+
+    private int bindLimitParametersAtStartOfQuery(RowSelection paramRowSelection, List params, int paramInt){
+
+    }
+
+    public int bindLimitParametersAtEndOfQuery(RowSelection paramRowSelection, List params, int paramInt){
+
+    }
+
+    public String getLimitSql(String sql, RowSelection rowSelection){
+        return this.dialect.getLimitSql(sql, rowSelection);
+    }
+
 }
--- nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseTenantCapacityMapper.java
+++ nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseTenantCapacityMapper.java
@@ -1,5 +1,7 @@
 package com.jn.nacos.plugin.datasource.base.mapper;
 
+import com.alibaba.nacos.common.utils.CollectionUtils;
+import com.alibaba.nacos.plugin.datasource.constants.FieldConstant;
 import com.alibaba.nacos.plugin.datasource.mapper.TenantCapacityMapper;
 import com.alibaba.nacos.plugin.datasource.model.MapperContext;
 import com.alibaba.nacos.plugin.datasource.model.MapperResult;
@@ -11,6 +13,11 @@ public class BaseTenantCapacityMapper extends BaseMapper implements TenantCapaci
 
     @Override
     public MapperResult getCapacityList4CorrectUsage(MapperContext context) {
-        return null;
+
+        String sql = "SELECT id, tenant_id FROM tenant_capacity WHERE id>?";
+        MapperResult result= new MapperResult(sql, CollectionUtils.list(context.getWhereParameter(FieldConstant.ID),
+                context.getWhereParameter(FieldConstant.LIMIT_SIZE)));
+
+        return result;
     }
 }
