026feb3edb7d2d79f30896974054ce4123c3ba73
coderjn <fs1194361820@163.com>
2024-08-23 23:06:26 +0800

    * 支持 configInfoAggrMapper

--- nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseConfigInfoAggrMapper.java
+++ nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseConfigInfoAggrMapper.java
@@ -1,8 +1,13 @@
 package com.jn.nacos.plugin.datasource.base.mapper;
 
+import com.alibaba.nacos.common.utils.CollectionUtils;
+import com.alibaba.nacos.plugin.datasource.constants.FieldConstant;
 import com.alibaba.nacos.plugin.datasource.mapper.ConfigInfoAggrMapper;
 import com.alibaba.nacos.plugin.datasource.model.MapperContext;
 import com.alibaba.nacos.plugin.datasource.model.MapperResult;
+import com.jn.sqlhelper.dialect.pagination.RowSelection;
+
+import java.util.List;
 
 public class BaseConfigInfoAggrMapper extends BaseMapper implements ConfigInfoAggrMapper {
     public BaseConfigInfoAggrMapper(String databaseId) {
@@ -11,6 +16,22 @@ public class BaseConfigInfoAggrMapper extends BaseMapper implements ConfigInfoAg
 
     @Override
     public MapperResult findConfigInfoAggrByPageFetchRows(MapperContext context) {
-        return null;
+        final int startRow = context.getStartRow();
+        final int pageSize = context.getPageSize();
+        final String dataId = (String) context.getWhereParameter(FieldConstant.DATA_ID);
+        final String groupId = (String) context.getWhereParameter(FieldConstant.GROUP_ID);
+        final String tenantId = (String) context.getWhereParameter(FieldConstant.TENANT_ID);
+
+        RowSelection rowSelection = new RowSelection(startRow, pageSize);
+        String sql =
+                "SELECT data_id,group_id,tenant_id,datum_id,app_name,content FROM config_info_aggr WHERE data_id=? AND "
+                        + "group_id=? AND tenant_id=? ORDER BY datum_id OFFSET " + startRow + " ROWS FETCH NEXT "
+                        + pageSize + " ROWS ONLY";
+        List<Object> paramList = CollectionUtils.list(dataId, groupId, tenantId);
+
+        sql = getDialect().getLimitSql(sql, rowSelection);
+
+        List pagedParams = getDialect().rebuildParameters(paramList, rowSelection);
+        return new MapperResult(sql, pagedParams);
     }
 }
--- nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseMapper.java
+++ nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseMapper.java
@@ -1,33 +1,31 @@
 package com.jn.nacos.plugin.datasource.base.mapper;
 
 import com.alibaba.nacos.plugin.datasource.mapper.AbstractMapper;
-import com.jn.langx.lifecycle.Initializable;
-import com.jn.langx.lifecycle.InitializationException;
 import com.jn.langx.util.Preconditions;
 import com.jn.nacos.plugin.datasource.base.dialect.DatabaseDialect;
 
-public abstract class BaseMapper extends AbstractMapper implements Initializable {
+public abstract class BaseMapper extends AbstractMapper{
     private String databaseId;
-    protected DatabaseDialect databaseDialect;
+    protected DatabaseDialect dialect;
 
     public BaseMapper(String databaseId){
         Preconditions.checkNotEmpty(databaseId,"database id is empty");
         this.databaseId = databaseId;
-        this.init();
+        this.dialect = null;
     }
 
-    @Override
-    public void init() throws InitializationException {
-        databaseDialect = null;
-    }
 
     @Override
     public String getDataSource() {
         return databaseId;
     }
 
+    public DatabaseDialect getDialect() {
+        return dialect;
+    }
+
     @Override
     public String getFunction(String functionName) {
-        return this.databaseDialect.getFunction(functionName);
+        return this.dialect.getFunction(functionName);
     }
 }
