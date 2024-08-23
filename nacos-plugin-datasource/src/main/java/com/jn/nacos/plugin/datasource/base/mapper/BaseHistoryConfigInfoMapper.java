5f10c717970f08ad3c93a43ebd2e8574ae95d091
coderjn <fs1194361820@163.com>
2024-08-24 07:59:18 +0800

    * 支持 HistoryConfigInfoMapper

--- nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseHistoryConfigInfoMapper.java
+++ nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseHistoryConfigInfoMapper.java
@@ -1,8 +1,13 @@
 package com.jn.nacos.plugin.datasource.base.mapper;
 
+import com.alibaba.nacos.common.utils.CollectionUtils;
+import com.alibaba.nacos.plugin.datasource.constants.FieldConstant;
 import com.alibaba.nacos.plugin.datasource.mapper.HistoryConfigInfoMapper;
 import com.alibaba.nacos.plugin.datasource.model.MapperContext;
 import com.alibaba.nacos.plugin.datasource.model.MapperResult;
+import com.jn.sqlhelper.dialect.pagination.RowSelection;
+
+import java.util.List;
 
 public class BaseHistoryConfigInfoMapper extends BaseMapper implements HistoryConfigInfoMapper {
     public BaseHistoryConfigInfoMapper(String databaseId) {
@@ -11,11 +16,51 @@ public class BaseHistoryConfigInfoMapper extends BaseMapper implements HistoryCo
 
     @Override
     public MapperResult removeConfigHistory(MapperContext context) {
-        return null;
+
+        int pageSize = Integer.parseInt(context.getWhereParameter(FieldConstant.LIMIT_SIZE).toString());
+        RowSelection rowSelection = new RowSelection(0, pageSize);
+
+        String subquery = "SELECT id FROM his_config_info WHERE gmt_modified < ? ";
+        String pagedSubquery = getDialect().getLimitSql(subquery, rowSelection);
+
+        String sql = "DELETE FROM his_config_info WHERE id IN( "+ pagedSubquery+ ")";
+
+        List paramList = CollectionUtils.list(context.getWhereParameter(FieldConstant.START_TIME));
+        List pagedParams = getDialect().rebuildParameters(paramList, rowSelection);
+        return new MapperResult(sql, pagedParams);
     }
 
     @Override
     public MapperResult pageFindConfigHistoryFetchRows(MapperContext context) {
-        return null;
+
+        RowSelection rowSelection = new RowSelection(context.getStartRow(), context.getPageSize());
+        String sql =
+                "SELECT nid,data_id,group_id,tenant_id,app_name,src_ip,src_user,op_type,gmt_create,gmt_modified FROM his_config_info "
+                        + "WHERE data_id = ? AND group_id = ? AND tenant_id = ? ORDER BY nid DESC  ";
+
+        sql = getDialect().getLimitSql(sql, rowSelection);
+
+        List paramList = CollectionUtils.list(context.getWhereParameter(FieldConstant.DATA_ID),
+                context.getWhereParameter(FieldConstant.GROUP_ID), context.getWhereParameter(FieldConstant.TENANT_ID));
+
+        List pagedParams = getDialect().rebuildParameters(paramList, rowSelection);
+        return new MapperResult(sql, pagedParams);
+    }
+
+    @Override
+    public MapperResult findDeletedConfig(MapperContext context) {
+
+        int pageSize = Integer.parseInt(context.getWhereParameter(FieldConstant.PAGE_SIZE).toString());
+        RowSelection rowSelection = new RowSelection(0, pageSize);
+
+        String sql = "SELECT data_id, group_id, tenant_id,gmt_modified,nid FROM his_config_info WHERE op_type = 'D' AND "
+                + "gmt_modified >= ? and nid > ? order by nid ";
+        sql = getDialect().getLimitSql(sql, rowSelection);
+
+        List paramList =CollectionUtils.list(context.getWhereParameter(FieldConstant.START_TIME),
+                context.getWhereParameter(FieldConstant.LAST_MAX_ID));
+
+        List pagedParams = getDialect().rebuildParameters(paramList, rowSelection);
+        return new MapperResult(sql, pagedParams);
     }
 }
