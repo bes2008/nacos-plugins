0d0f56a3582a1af06f500ad48e5ce7787ea6c5f1
coderjn <fs1194361820@163.com>
2024-08-24 07:47:39 +0800

    * 支持 GroupCapacityMapper

--- nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseGroupCapacityMapper.java
+++ nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseGroupCapacityMapper.java
@@ -1,8 +1,13 @@
 package com.jn.nacos.plugin.datasource.base.mapper;
 
+import com.alibaba.nacos.common.utils.CollectionUtils;
+import com.alibaba.nacos.plugin.datasource.constants.FieldConstant;
 import com.alibaba.nacos.plugin.datasource.mapper.GroupCapacityMapper;
 import com.alibaba.nacos.plugin.datasource.model.MapperContext;
 import com.alibaba.nacos.plugin.datasource.model.MapperResult;
+import com.jn.sqlhelper.dialect.pagination.RowSelection;
+
+import java.util.List;
 
 public class BaseGroupCapacityMapper extends BaseMapper implements GroupCapacityMapper {
     public BaseGroupCapacityMapper(String databaseId) {
@@ -11,6 +16,11 @@ public class BaseGroupCapacityMapper extends BaseMapper implements GroupCapacity
 
     @Override
     public MapperResult selectGroupInfoBySize(MapperContext context) {
-        return null;
+        RowSelection rowSelection = new RowSelection(0, context.getPageSize());
+        String sql = "SELECT id, group_id FROM group_capacity WHERE id > ? ";
+        sql = getDialect().getLimitSql(sql, rowSelection);
+        List paramList =  CollectionUtils.list(context.getWhereParameter(FieldConstant.ID));
+        List pagedParams = getDialect().rebuildParameters(paramList, rowSelection);
+        return new MapperResult(sql, pagedParams);
     }
 }
