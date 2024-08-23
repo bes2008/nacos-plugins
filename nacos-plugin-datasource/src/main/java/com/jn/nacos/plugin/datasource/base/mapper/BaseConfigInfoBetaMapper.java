c56dbd4a086efcd6b1d6a8426854c93d4d0575a1
coderjn <fs1194361820@163.com>
2024-08-24 07:02:29 +0800

    * 支持 ConfigInfoBetaMapper

--- nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseConfigInfoBetaMapper.java
+++ nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseConfigInfoBetaMapper.java
@@ -3,6 +3,10 @@ package com.jn.nacos.plugin.datasource.base.mapper;
 import com.alibaba.nacos.plugin.datasource.mapper.ConfigInfoBetaMapper;
 import com.alibaba.nacos.plugin.datasource.model.MapperContext;
 import com.alibaba.nacos.plugin.datasource.model.MapperResult;
+import com.google.common.collect.Lists;
+import com.jn.sqlhelper.dialect.pagination.RowSelection;
+
+import java.util.List;
 
 public class BaseConfigInfoBetaMapper extends BaseMapper implements ConfigInfoBetaMapper {
     public BaseConfigInfoBetaMapper(String databaseId) {
@@ -11,6 +15,18 @@ public class BaseConfigInfoBetaMapper extends BaseMapper implements ConfigInfoBe
 
     @Override
     public MapperResult findAllConfigInfoBetaForDumpAllFetchRows(MapperContext context) {
-        return null;
+        int startRow = context.getStartRow();
+        int pageSize = context.getPageSize();
+
+        RowSelection rowSelection = new RowSelection(startRow, pageSize);
+        String subquery= "SELECT id FROM config_info_beta ORDER BY id ";
+        subquery = getDialect().getLimitSql(subquery, rowSelection);
+        String sql = "SELECT t.id,data_id,group_id,tenant_id,app_name,content,md5,gmt_modified,beta_ips "
+                + " FROM ( "+subquery+" )" + " g, config_info_beta t WHERE g.id = t.id";
+
+        List<Object> paramList = Lists.newArrayList();
+        List pagedParams = getDialect().rebuildParameters(paramList, rowSelection);
+
+        return new MapperResult(sql, paramList);
     }
 }
