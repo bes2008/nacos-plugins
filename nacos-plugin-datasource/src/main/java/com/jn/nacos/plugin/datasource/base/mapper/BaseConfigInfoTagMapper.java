bc8f2de6f79e1c8fd7663e530ef8c03c41d0ce28
coderjn <fs1194361820@163.com>
2024-08-24 07:41:27 +0800

    * 支持 ConfigInfoTagMapper

--- nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseConfigInfoTagMapper.java
+++ nacos-plugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/base/mapper/BaseConfigInfoTagMapper.java
@@ -3,6 +3,10 @@ package com.jn.nacos.plugin.datasource.base.mapper;
 import com.alibaba.nacos.plugin.datasource.mapper.ConfigInfoTagMapper;
 import com.alibaba.nacos.plugin.datasource.model.MapperContext;
 import com.alibaba.nacos.plugin.datasource.model.MapperResult;
+import com.jn.sqlhelper.dialect.pagination.RowSelection;
+
+import java.util.Collections;
+import java.util.List;
 
 public class BaseConfigInfoTagMapper extends BaseMapper implements ConfigInfoTagMapper {
     public BaseConfigInfoTagMapper(String databaseId) {
@@ -11,6 +15,13 @@ public class BaseConfigInfoTagMapper extends BaseMapper implements ConfigInfoTag
 
     @Override
     public MapperResult findAllConfigInfoTagForDumpAllFetchRows(MapperContext context) {
-        return null;
+        RowSelection rowSelection = new RowSelection(context.getStartRow(), context.getPageSize());
+        String subquery= " SELECT id FROM config_info_tag  ORDER BY id ";
+        String pagedSubquery = getDialect().getLimitSql(subquery, rowSelection);
+        String sql = "SELECT t.id,data_id,group_id,tenant_id,tag_id,app_name,content,md5,gmt_modified FROM ( "+pagedSubquery+" ) g, config_info_tag t  WHERE g.id = t.id";
+        List paramList = Collections.emptyList();
+        List pagedParams = getDialect().rebuildParameters(paramList,rowSelection);
+
+        return new MapperResult(sql, pagedParams);
     }
 }
