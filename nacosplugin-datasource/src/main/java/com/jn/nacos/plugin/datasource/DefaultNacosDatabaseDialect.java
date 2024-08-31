863c1a8a3310f72dfc9da647197bf43e272ba7d5
coderjn <fs1194361820@163.com>
2024-08-31 11:42:39 +0800

    * 代码整理

--- nacosplugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/DefaultNacosDatabaseDialect.java
+++ nacosplugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/DefaultNacosDatabaseDialect.java
@@ -1,6 +1,6 @@
 package com.jn.nacos.plugin.datasource;
 
-public class DefaultNacosDatabaseDialect extends NacosDatabaseDialect{
+class DefaultNacosDatabaseDialect extends NacosDatabaseDialect{
     public DefaultNacosDatabaseDialect(String name) {
         super(name);
     }
