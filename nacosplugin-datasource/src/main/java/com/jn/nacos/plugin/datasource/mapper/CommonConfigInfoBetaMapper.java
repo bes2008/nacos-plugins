49bace19c658972ae669f86f83b72c13fdf9f904
fjn1194361820 <fs1194361820@163.com>
2024-09-06 14:04:53 +0800

    * 更新 ConfigInfoBetaMapper

--- nacosplugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/mapper/CommonConfigInfoBetaMapper.java
+++ nacosplugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/mapper/CommonConfigInfoBetaMapper.java
@@ -1,13 +1,11 @@
 package com.jn.nacos.plugin.datasource.mapper;
 
-import com.alibaba.nacos.plugin.datasource.constants.FieldConstant;
 import com.alibaba.nacos.plugin.datasource.mapper.ConfigInfoBetaMapper;
 import com.alibaba.nacos.plugin.datasource.model.MapperContext;
 import com.alibaba.nacos.plugin.datasource.model.MapperResult;
 import com.jn.langx.util.collection.Lists;
 import com.jn.sqlhelper.dialect.pagination.RowSelection;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CommonConfigInfoBetaMapper extends BaseMapper implements ConfigInfoBetaMapper {
@@ -34,27 +32,7 @@ public class CommonConfigInfoBetaMapper extends BaseMapper implements ConfigInfo
 
     @Override
     public MapperResult updateConfigInfo4BetaCas(MapperContext context) {
-
-        List<String> where = Lists.newArrayList("data_id", "group_id", "tenant_id");
-        final String sql = "UPDATE config_info_beta SET content = ?,md5 = ?,beta_ips = ?,src_ip = ?,src_user = ?,gmt_modified = "
-                + getFunction("NOW()")
-                + ",app_name = ? " + genWhereClause(where)
-                + " AND (md5 = ? OR md5 is null OR md5 = '')";
-
-        List<Object> paramList = new ArrayList<>();
-
-        paramList.add(context.getUpdateParameter(FieldConstant.CONTENT));
-        paramList.add(context.getUpdateParameter(FieldConstant.MD5));
-        paramList.add(context.getUpdateParameter(FieldConstant.BETA_IPS));
-        paramList.add(context.getUpdateParameter(FieldConstant.SRC_IP));
-        paramList.add(context.getUpdateParameter(FieldConstant.SRC_USER));
-        paramList.add(context.getUpdateParameter(FieldConstant.APP_NAME));
-
-        paramList.add(context.getWhereParameter(FieldConstant.DATA_ID));
-        paramList.add(context.getWhereParameter(FieldConstant.GROUP_ID));
-        paramList.add(context.getWhereParameter(FieldConstant.TENANT_ID));
-        paramList.add(context.getWhereParameter(FieldConstant.MD5));
-
-        return new MapperResult(sql, paramList);
+        useDefaultTenantIdWithWhereParameter(context);
+        return ConfigInfoBetaMapper.super.updateConfigInfo4BetaCas(context);
     }
 }
