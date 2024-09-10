63682a86c6917a3cbea1e5edb6bd1369b039c36b
fjn1194361820 <fs1194361820@163.com>
2024-09-10 18:11:07 +0800

    * 调整oracle默认方案

--- nacosplugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/db/oracle/OracleDatabaseDialect.java
+++ nacosplugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/db/oracle/OracleDatabaseDialect.java
@@ -7,7 +7,7 @@ import com.jn.nacos.plugin.datasource.NacosDatabaseDialect;
 public class OracleDatabaseDialect extends NacosDatabaseDialect {
     public OracleDatabaseDialect(){
         super(DatabaseNames.ORACLE);
-        this.identifierQuotedMode = IdentifierQuotedMode.MIXED;
+        this.identifierQuotedMode = IdentifierQuotedMode.UNQUOTED;
     }
 
     @Override
