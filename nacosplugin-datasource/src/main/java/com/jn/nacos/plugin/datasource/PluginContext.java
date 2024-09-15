02868c285237217d5995eb1a6595ed0ba2a3309d
coderjn <fs1194361820@163.com>
2024-09-15 19:29:29 +0800

    * 代码整理

--- nacosplugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/PluginContext.java
+++ nacosplugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/PluginContext.java
@@ -1,18 +1,32 @@
 package com.jn.nacos.plugin.datasource;
 
+import com.jn.langx.lifecycle.AbstractInitializable;
+import com.jn.langx.lifecycle.InitializationException;
 import com.jn.langx.util.Objs;
 import com.jn.langx.util.Preconditions;
+import com.jn.langx.util.logging.Loggers;
+import org.slf4j.Logger;
 
-public class PluginContext {
+public class PluginContext extends AbstractInitializable {
     private String databaseName;
     protected NacosDatabaseDialect dialect;
     private IdentifierQuotedMode identifierQuotedModeInDDL;
     private PluginContext(){
+       init();
+    }
+
+    @Override
+    protected void doInit() throws InitializationException {
+        Logger logger = Loggers.getLogger(PluginContext.class);
+        logger.info("==================nacos datasource plugin initial==================");
         this.databaseName = NacosEnvs.getConfiguredDatabaseName();
         Preconditions.checkTrue(!Objs.equals(DatabaseNames.UNSUPPORTED, this.databaseName), "database {} is unsupported", this.databaseName);
         this.dialect = NacosDatabaseDialectManager.getInstance().getDialect(this.databaseName);
         this.identifierQuotedModeInDDL = NacosEnvs.getConfiguredIdentifierQuotedMode(this.dialect);
+        logger.info("dialect: {}, identifierQuotedMode: {}", this.databaseName, this.identifierQuotedModeInDDL);
+        logger.info("==================nacos datasource plugin initial finished ==================");
     }
+
     public static final PluginContext INSTANCE = new PluginContext();
 
     public String getDatabaseName() {
