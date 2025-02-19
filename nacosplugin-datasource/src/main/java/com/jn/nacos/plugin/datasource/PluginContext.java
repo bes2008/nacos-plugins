0e6bb42a2f123bb8e2f3e45b207b755a3b71710d
fjn1194361820 <fs1194361820@163.com>
2025-02-19 18:43:29 +0800

    * update it

--- nacosplugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/PluginContext.java
+++ nacosplugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/PluginContext.java
@@ -48,7 +48,7 @@ public class PluginContext extends AbstractInitializable {
             this.identifierQuotedModeInDDL = NacosEnvs.getIdentifierQuotedMode(this.dialect);
             this.sqlCompatibilityType = NacosEnvs.getSqlCompatibilityType(this.dialect);
         }
-        logger.info("dialect: {}, identifierQuotedMode: {}, sqlCompatibilityType: {}", this.databaseName, this.identifierQuotedModeInDDL, this.sqlCompatibilityType.getName());
+        logger.info("dialect: {}, identifierQuotedMode: {}, sqlCompatibilityType: {}", this.databaseName, this.identifierQuotedModeInDDL, this.sqlCompatibilityType==null?null: this.sqlCompatibilityType.getName());
         logger.info("================== nacos datasource plugin context initial finished ==================");
     }
 
