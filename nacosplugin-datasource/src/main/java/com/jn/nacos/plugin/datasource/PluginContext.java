a3f56d68cd259d9a1685666b809ce161298d315d
coderjn <fs1194361820@163.com>
2025-12-19 21:50:23 +0800

    * update it

--- nacosplugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/PluginContext.java
+++ nacosplugin-datasource/src/main/java/com/jn/nacos/plugin/datasource/PluginContext.java
@@ -47,7 +47,7 @@ public class PluginContext extends AbstractInitializable {
             this.dialect = NacosDatabaseDialectManager.getInstance().getDialect(this.databaseName);
             this.identifierQuotedModeInDDL = NacosEnvs.getIdentifierQuotedMode(this.dialect);
             this.sqlCompatibilityType = NacosEnvs.getSqlCompatibilityType(this.dialect);
-            logger.info("dialect: {}, identifierQuotedMode: {}, sqlCompatibilityType: {}", this.databaseName, this.identifierQuotedModeInDDL, this.sqlCompatibilityType.getName());
+            logger.info("selected dialect: {}, identifierQuotedMode: {}, sqlCompatibilityType: {}", this.databaseName, this.identifierQuotedModeInDDL, this.sqlCompatibilityType.getName());
         }
         logger.info("================== nacos datasource plugin context initial finished ==================");
     }
